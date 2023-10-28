4b0ea7f6ecd74e917399b6075bb666eb56f9fa10
cianmung <cianmung1999@gmail.com>
2023-10-28 16:16:11 +0800

    Finished Lab 3

---
+++ lab-3-2.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+void reverseAr1D(int ar[], int size);
+
+int main()
+{
+    int ar[10];
+    int size, i;
+
+    printf("Enter array size: \n");
+    scanf("%d", &size);
+    printf("Enter %d data \n", size);
+
+    for(i=0; i <= size-1; i++)
+        scanf("%d", &ar[i]);
+    reverseAr1D(ar, size);
+    printf("reverseAr1D(): ");
+
+    if (size > 0)
+    {
+        for(i=0; i<size; i++)
+            printf("%d ", ar[i]);
+    }
+
+    return 0;
+}
+
+void reverseAr1D(int ar[], int size)
+{
+    int i;
+    int tempAr[10];
+    for (i=0; i<size; i++)
+    {
+        tempAr[i] = ar[size-1-i];
+    }
+
+    for (i=0; i<size; i++)
+    {
+        ar[i] = tempAr[i];
+
+    }
+}
